21cc1b29b2a69b1d440f4e10336cb9204984066a
A.I.Manik <is@not.you>
2024-11-24 14:14:35 +0300

    Rejected tasks handling

--- src/main/java/com/manik/multithreading/threadReusability/threadpool/Main.java
+++ src/main/java/com/manik/multithreading/threadReusability/threadpool/Main.java
@@ -7,75 +7,50 @@ import java.util.concurrent.*;
 public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutorExtention(
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 2,
-                2,
+                3,
                 1,
                 TimeUnit.MINUTES,
-                new ArrayBlockingQueue<>(2)
+                new ArrayBlockingQueue<>(2),
+                new ThreadPoolExecutor.DiscardPolicy()
         );
 
+        System.out.println("[1] Thread pool size: " + threadPoolExecutor.getPoolSize());
 
-        Thread fread = new Thread(() -> { throw new RuntimeException("Hello");});
-        fread.setUncaughtExceptionHandler((t, e) -> {
-            System.out.println("i'm from exception handler");
-        });
-        threadPoolExecutor.execute(fread);
-        /*Future<String> future = threadPoolExecutor.submit(() -> {
-           throw new RuntimeException("Hello");
-        });
+        threadPoolExecutor.submit(new SleepingTask(1));
+        threadPoolExecutor.submit(new SleepingTask(2));
 
+        System.out.println("[2] Thread pool size: " + threadPoolExecutor.getPoolSize());
 
-        try {
-            future.get();
-        } catch (InterruptedException | ExecutionException | RuntimeException e) {
-            e.printStackTrace();
-        }*/
+        threadPoolExecutor.submit(new SleepingTask(3));
+        threadPoolExecutor.submit(new SleepingTask(4));
 
-        //threadPoolExecutor.shutdown();
+        threadPoolExecutor.submit(new SleepingTask(5));
+        System.out.println("[3] Thread pool size: " + threadPoolExecutor.getPoolSize());
 
+        System.out.println("Queue size: " + threadPoolExecutor.getQueue().remainingCapacity());
+        threadPoolExecutor.submit(new SleepingTask(6));
+        threadPoolExecutor.submit(new SleepingTask(7));
     }
 
-    static class ThreadPoolExecutorExtention extends ThreadPoolExecutor {
+    static class SleepingTask implements Runnable {
 
-        public ThreadPoolExecutorExtention(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
-            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-        }
+        private final Integer taskId;
 
-        public ThreadPoolExecutorExtention(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
-            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
-        }
-
-        public ThreadPoolExecutorExtention(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
-            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+        public SleepingTask(Integer taskId) {
+            this.taskId = taskId;
         }
 
         @Override
-        protected void afterExecute(Runnable r, Throwable t) {
-            //super.afterExecute(r, t);
+        public void run() {
 
-            if (r instanceof Future) {
-                try{
-                    Future<String> future = (Future) r;
-                    future.get();
-                } catch (Exception e){
-                    System.out.println("It is future");
-                }
-            } else if (t != null) {
-                System.out.println("Hello from afterExecute");
+            System.out.println("Task id: " + taskId);
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }
-
-        public ThreadPoolExecutorExtention(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
-            super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
-        }
-    }
-
-    static class CallableTask implements Callable<String> {
-
-        @Override
-        public String call() throws Exception {
-            return "Pass";
         }
     }
 }
