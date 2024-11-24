76b469bed42a75d0c9761c11d6bdcbbce3a2bf1d
A.I.Manik <is@not.you>
2024-11-24 18:50:53 +0300

    Base thread pool monitoring metrics

--- src/main/java/com/manik/multithreading/threadReusability/threadpool/Main.java
+++ src/main/java/com/manik/multithreading/threadReusability/threadpool/Main.java
@@ -10,28 +10,41 @@ public class Main {
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
                 2,
                 3,
-                1,
-                TimeUnit.MINUTES,
+                20,
+                TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(2),
                 new ThreadPoolExecutor.DiscardPolicy()
         );
 
         System.out.println("[1] Thread pool size: " + threadPoolExecutor.getPoolSize());
-
+        System.out.println("[1] Active tasks: " + threadPoolExecutor.getActiveCount());
         threadPoolExecutor.submit(new SleepingTask(1));
         threadPoolExecutor.submit(new SleepingTask(2));
 
         System.out.println("[2] Thread pool size: " + threadPoolExecutor.getPoolSize());
+        System.out.println("[2] Active tasks: " + threadPoolExecutor.getActiveCount());
 
         threadPoolExecutor.submit(new SleepingTask(3));
         threadPoolExecutor.submit(new SleepingTask(4));
+        System.out.println("[3] Active tasks: " + threadPoolExecutor.getActiveCount());
 
         threadPoolExecutor.submit(new SleepingTask(5));
         System.out.println("[3] Thread pool size: " + threadPoolExecutor.getPoolSize());
+        System.out.println("[4] Active tasks: " + threadPoolExecutor.getActiveCount());
 
         System.out.println("Queue size: " + threadPoolExecutor.getQueue().remainingCapacity());
         threadPoolExecutor.submit(new SleepingTask(6));
         threadPoolExecutor.submit(new SleepingTask(7));
+
+        threadPoolExecutor.allowsCoreThreadTimeOut();
+        while(true){
+            System.out.println("[5] Active tasks: " + threadPoolExecutor.getActiveCount());
+            System.out.println("Task count " + threadPoolExecutor.getTaskCount());
+            System.out.println("Completed tasks " + threadPoolExecutor.getCompletedTaskCount());
+            System.out.println("Pool size " + threadPoolExecutor.getPoolSize());
+            Thread.sleep(1000);
+        }
+
     }
 
     static class SleepingTask implements Runnable {
